bf94796c7c4e1dc8dd7210bc3942570d88420ad0
cgavernet <carlosgavernet@gmail.com>
2023-06-30 15:18:35 -0300

    create & rename funtions on validators.js

--- src/middleware/validators.js
+++ src/middleware/validators.js
@@ -1,8 +1,8 @@
 const { body, validationResult } = require('express-validator');
 
-const userValidator = [
-    body('firstname').notEmpty(),
-    body('lastname').notEmpty(),
+const createUserValidator = [
+    body('firstName').notEmpty(),
+    body('lastName').notEmpty(),
     body('email').isEmail(),
     body('username').notEmpty(),
     body('password').isLength({ min: 8 }),
@@ -15,12 +15,39 @@ const userValidator = [
     },
 ];
 
-const bookValidator = [
-    body().isJSON(),
-    body('isbn').notEmpty(),
+const updateUserValidator = [
+    body('firstName')
+        .optional()
+        .notEmpty(),
+    body('lastName')
+        .optional()
+        .notEmpty(),
+    body('email')
+        .optional()
+        .isEmail(),
+    body('username')
+        .optional()
+        .notEmpty(),
+    body('password')
+        .optional()
+        .isLength({ min: 8 }),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+];
+
+const createBookValidator = [
+    body('isbn')
+        .notEmpty()
+        .isNumeric(),
     body('titulo').notEmpty(),
     body('autor').notEmpty(),
-    body('year').notEmpty(),    
+    body('year').notEmpty(),
+    body('library').notEmpty(),    
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -30,11 +57,23 @@ const bookValidator = [
     }
 ];
 
-const libraryValidator = [
-    body().isJSON(),
-    body('name').isEmpty(),
-    body('location').isEmpty(),
-    body('telefono').isEmpty(),
+const updateBookValidator = [
+    body('isbn')
+        .optional()
+        .notEmpty()
+        .isNumeric(),
+    body('titulo')
+        .optional()
+        .notEmpty(),
+    body('autor')
+        .optional()
+        .notEmpty(),
+    body('year')
+        .optional()
+        .notEmpty(),
+    body('library')
+        .optional()
+        .notEmpty(),    
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -42,7 +81,37 @@ const libraryValidator = [
         }
         next();
     }
-
 ];
 
-module.exports = { userValidator, bookValidator, libraryValidator };
+const createLibraryValidator = [
+    body('name').notEmpty(),
+    body('location').notEmpty(),
+    body('telefono').notEmpty(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
+
+const updateLibraryValidator = [
+    body('name')
+        .optional()
+        .notEmpty(),
+    body('location')
+        .optional()
+        .notEmpty(),
+    body('telefono')
+        .optional()
+        .notEmpty(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
+module.exports = { createUserValidator, updateUserValidator, createBookValidator, updateBookValidator, createLibraryValidator, updateLibraryValidator };
