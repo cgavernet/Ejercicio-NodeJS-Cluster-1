4844784f0140a88a72481221da505f09f0ef3830
cgavernet <carlosgavernet@gmail.com>
2023-06-30 12:46:32 -0300

    add libraryValidator in validators.js

--- src/middleware/validators.js
+++ src/middleware/validators.js
@@ -30,4 +30,19 @@ const bookValidator = [
     }
 ];
 
-module.exports = { userValidator, bookValidator };
+const libraryValidator = [
+    body().isJSON(),
+    body('name').isEmpty(),
+    body('location').isEmpty(),
+    body('telefono').isEmpty(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+
+];
+
+module.exports = { userValidator, bookValidator, libraryValidator };
