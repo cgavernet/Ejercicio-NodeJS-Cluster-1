0d7ce15ab42e1486f5887c4c88eb99e7f774550c
cgavernet <carlosgavernet@gmail.com>
2023-06-30 11:04:52 -0300

    add validators middleware

---
+++ src/middleware/validators.js
@@ -0,0 +1,33 @@
+const { body, validationResult } = require('express-validator');
+
+const userValidator = [
+    body('firstname').notEmpty(),
+    body('lastname').notEmpty(),
+    body('email').isEmail(),
+    body('username').notEmpty(),
+    body('password').isLength({ min: 8 }),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+];
+
+const bookValidator = [
+    body().isJSON(),
+    body('isbn').notEmpty(),
+    body('titulo').notEmpty(),
+    body('autor').notEmpty(),
+    body('year').notEmpty(),    
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
+
+module.exports = { userValidator, bookValidator };
